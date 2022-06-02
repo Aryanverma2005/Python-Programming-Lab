5087cf659904b4f4be30e3bd2d450047c4af8c63
Aryanverma2005 <92083434+Aryanverma2005@users.noreply.github.com>
2022-06-02 11:23:24 +0530

    pattern 4

---
+++ file4.py
@@ -0,0 +1,4 @@
+f = open('sample.txt')
+out = f.read()
+print(out)
+f.close()
---
+++ patten4.py
@@ -0,0 +1,7 @@
+num = 5
+for i in range(num):
+    for j in range(i):
+        print(' ',end='')
+    for j in range(num-i):
+        print('*',end='')
+    print()
